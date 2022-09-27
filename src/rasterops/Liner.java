f7a1c5bc880c56905cb31ee03c60920b4a715035
MK <konvima1@uhk.cz>
2022-09-27 15:41:43 +0200

    Add doc comment to the drawLine function in the Liner interface

--- src/rasterops/Liner.java
+++ src/rasterops/Liner.java
@@ -10,13 +10,13 @@ import rasterdata.RasterImage;
 public interface Liner<P> {
 
     /**
-     *
-     * @param img
-     * @param c1
-     * @param r1
-     * @param c2
-     * @param r2
-     * @param pixelValue
+     * Draws a line onto the given {@link  RasterImage} with the given pixel
+     * @param img {@link RasterImage} to be used for drawing
+     * @param c1 column address of the start point
+     * @param r1 row address of the start point
+     * @param c2 column address of the end point
+     * @param r2 row address of the end point
+     * @param pixelValue new pixel value
      */
     void drawLine(final @NotNull RasterImage<P> img, final int c1,
                   final int r1, final int c2, final int r2, final @NotNull P pixelValue);
