c91e536abc2e2d80a55f127c83acaa5a2496a7cc
Tom <zampato1@uhk.cz>
2022-10-15 12:02:31 +0200

    a

--- src/rasterops/TrivialLiner.java
+++ src/rasterops/TrivialLiner.java
@@ -3,6 +3,9 @@ package rasterops;
 import org.jetbrains.annotations.NotNull;
 import rasterdata.RasterImage;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
+
 public class TrivialLiner<P> implements Liner<P>{
 
 
@@ -11,13 +14,21 @@ public class TrivialLiner<P> implements Liner<P>{
                          final @NotNull P pixelValue) {
 
 
+        Stroke drawingStroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+        Line2D line = new Line2D.Double(20, 40, 100, 40);
 
         double deltaY = y2 - y1;
         double deltaX = x2 - x1;
+
         if (Math.abs(deltaY) >= Math.abs(deltaX)) {
             if ((deltaX == 0) && (deltaY == 0)) {
                 img.setPixel(x1, y1, pixelValue);
             } else {
+                img.setPixel(x,y,pixelValue);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setStroke(drawingStroke);
+                g2d.draw(line);
+
                 if (y1 > y2) {
                     int pom = x2;
                     x2 = x1;
@@ -45,11 +56,17 @@ public class TrivialLiner<P> implements Liner<P>{
                 y2 = y1;
                 y1 = pom;
             }
+            img.setPixel(x,y,pixelValue);
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setStroke(drawingStroke);
+            g2d.draw(line);
+
             double k = (double) (deltaY / deltaX);
             double q = y1 - k * x1;
             for (int x = x1; x <= x2; x++) {
                 int y = (int) (k * x + q);
                 img.setPixel(x, y, pixelValue);
+
             }
         }
     }
